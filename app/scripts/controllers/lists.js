59ba6d65f48293cc8af06e319a0d52231a7230bd
Julian Poyourow <julianpoyo@gmail.com>
2016-02-21 11:50:00 -0800

    Updated lists view

--- app/scripts/controllers/lists.js
+++ app/scripts/controllers/lists.js
@@ -1,8 +1,12 @@
 angular.module('starter.controllers')
 
-.controller('ListsCtrl', function($scope) {
+.controller('ListsCtrl', function($scope, $state) {
   $scope.lists = [
     { title: 'Family Shopping', id: 1, people: {} },
     { title: 'School Project', id: 2, people: {} }
   ];
+
+  $scope.openList = function(id){
+    $state.go('app.list', {"listId": id});
+  }
 });
