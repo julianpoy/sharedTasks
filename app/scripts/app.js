29d15d35000ba59e49ce870f0f3ddf41dae4bdf3
Julian Poyourow <julianpoyo@gmail.com>
2016-02-21 11:29:55 -0800

    Moved around generated files
    Making app more fitted towards todo

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,42 +32,25 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('app.lists', {
+    url: '/lists',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/lists.html',
+        controller: 'ListsCtrl'
       }
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
+  .state('app.list', {
+    url: '/lists/:listId',
     views: {
       'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+        templateUrl: 'templates/list.html',
+        controller: 'ListCtrl'
       }
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/lists');
 });
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -41,16 +41,12 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+.controller('ListsCtrl', function($scope) {
+  $scope.lists = [
+    { title: 'Family Shopping', id: 1, people: {} },
+    { title: 'School Project', id: 2, people: {} }
   ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('ListCtrl', function($scope, $stateParams) {
 });
